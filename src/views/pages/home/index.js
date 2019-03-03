c82c74cfaabaeeee02113bdc1dd994687c9140ae
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-03-03 16:39:27 +0530

    React Redux starting project structure!

---
+++ src/views/pages/home/index.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const Home = () => <Fragment>
+  <div className="grid-container fluid">
+    <div className="grid-x grid-margin-x">
+      <div className="cell">
+        <h1>App Homepage</h1>
+      </div>
+    </div>
+  </div>
+</Fragment>;
+
+export default Home;
